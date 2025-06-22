a06ee65d4ee83b48b3146cc5ef7b0da5ed447d2c
tania <taniarajput303@gmail.com>
2025-06-22 15:53:59 +0530

    update

--- main.js
+++ main.js
@@ -14,19 +14,24 @@ let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
-    sections.forEach(sec=>{
-        let top=window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+   sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height){
-            navLinks.forEach.apply(links =>{
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*-' + id + ']').classList.add('active');
-            });
-        };
-    });
+    if (top >= offset && top < offset + height) {
+        navLinks.forEach(link => {
+            link.classList.remove('active');
+        });
+
+        let activeLink = document.querySelector('header nav a[href*="' + id + '"]');
+        if (activeLink) {
+            activeLink.classList.add('active');
+        }
+    }
+});
+
     // sticky navbar
 
     let header = document.querySelector('header');
